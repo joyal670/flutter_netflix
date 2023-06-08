5a79bd327c4c3e748c555a104affaab910afe294
Joyal <joyal670@gmail.com>
2023-06-08 11:42:00 +0530

    added screens

--- lib/core/dims/dims.dart
+++ lib/core/dims/dims.dart
@@ -16,3 +16,8 @@ const height20 = SizedBox(
 );
 
 BorderRadius radius10 = BorderRadius.circular(20);
+const mainImage =
+    'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg';
+
+TextStyle homeTitleTextStyle =
+    const TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,36 +1,201 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:netflix/core/colors/colors.dart';
+import 'package:netflix/core/dims/dims.dart';
 
 import 'MainTitleCard.dart';
 import 'NumberWidget.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  ValueNotifier<bool> scrollNotifier = ValueNotifier(true);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: const [
-              MainTitleCard(
-                title: 'Released in past years',
+        body: ValueListenableBuilder(
+            valueListenable: scrollNotifier,
+            builder: (BuildContext context, index, _) {
+              return NotificationListener<UserScrollNotification>(
+                onNotification: (notification) {
+                  final ScrollDirection direction = notification.direction;
+
+                  if (direction == ScrollDirection.reverse) {
+                    scrollNotifier.value = false;
+                  } else if (direction == ScrollDirection.forward) {
+                    scrollNotifier.value = true;
+                  }
+                  return true;
+                },
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ListView(
+                        children: [
+                          backgroundCard(),
+                          const MainTitleCard(
+                            title: 'Released in past years',
+                          ),
+                          const MainTitleCard(
+                            title: 'Tending Now',
+                          ),
+                          const NumberWidget(),
+                          const MainTitleCard(
+                            title: 'Tense Dramas',
+                          ),
+                          const MainTitleCard(
+                            title: 'South Indian Cinema',
+                          )
+                        ],
+                      ),
+                    ),
+                    scrollNotifier.value == true
+                        ? AnimatedContainer(
+                            duration: const Duration(milliseconds: 1000),
+                            child: Container(
+                              width: double.infinity,
+                              height: 90,
+                              color: Colors.black.withOpacity(0.5),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Image.network(
+                                          'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/2048px-Netflix_icon.svg.png',
+                                          width: 50,
+                                          height: 50),
+                                      const Spacer(),
+                                      IconButton(
+                                          onPressed: () {},
+                                          iconSize: 30,
+                                          icon: const Icon(
+                                            Icons.cast,
+                                            color: Colors.white,
+                                          )),
+                                      kWidth,
+                                      Container(
+                                        width: 30,
+                                        height: 30,
+                                        color: Colors.blue,
+                                      ),
+                                      kWidth
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 10),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Text(
+                                          'Tv Shows',
+                                          style: homeTitleTextStyle,
+                                        ),
+                                        Text(
+                                          'Movies',
+                                          style: homeTitleTextStyle,
+                                        ),
+                                        Text(
+                                          'Categories',
+                                          style: homeTitleTextStyle,
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        : kHeight
+                  ],
+                ),
+              );
+            }));
+  }
+
+  Stack backgroundCard() {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 500,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover, image: NetworkImage(mainImage))),
+        ),
+        Positioned(
+          bottom: 10,
+          left: 0,
+          right: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const CustomButtonWidget(
+                iconData: Icons.add,
+                title: 'My List',
               ),
-              MainTitleCard(
-                title: 'Tending Now',
-              ),
-              NumberWidget(),
-              MainTitleCard(
-                title: 'Tense Dramas',
-              ),
-              MainTitleCard(
-                title: 'South Indian Cinema',
+              playButton(),
+              const CustomButtonWidget(
+                iconData: Icons.info,
+                title: 'Info',
               )
             ],
           ),
+        )
+      ],
+    );
+  }
+
+  TextButton playButton() {
+    return TextButton.icon(
+      style:
+          ButtonStyle(backgroundColor: MaterialStateProperty.all(colorWhite)),
+      onPressed: () {},
+      icon: const Icon(
+        Icons.play_arrow,
+        size: 25,
+        color: colorBlack,
+      ),
+      label: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Text(
+          'Play',
+          style: TextStyle(fontSize: 20, color: colorBlack),
         ),
       ),
+    );
+  }
+}
+
+class CustomButtonWidget extends StatelessWidget {
+  final IconData iconData;
+  final String title;
+  const CustomButtonWidget({
+    super.key,
+    required this.iconData,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          iconData,
+          color: colorWhite,
+          size: 25,
+        ),
+        Text(
+          title,
+          style: const TextStyle(
+            color: colorWhite,
+            fontSize: 18,
+          ),
+        )
+      ],
     );
   }
 }
